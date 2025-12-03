616b47b5e41efde7cae3fb84b293211791cf6c97
Elias Avellaneda <elias.avellaneda@infocorpgroup.com>
2025-12-03 11:05:28 -0300

    Convertir a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,39 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
   // Cast process to any to avoid TS error: Property 'cwd' does not exist on type 'Process'
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+          name: 'DentiApp',
+          short_name: 'DentiApp',
+          description: 'Tu sonrisa, nuestra pasión.',
+          theme_color: '#0ea5e9',
+          background_color: '#ffffff',
+          display: 'standalone',
+          start_url: '/',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
     css: {
       postcss: {
         plugins: [
